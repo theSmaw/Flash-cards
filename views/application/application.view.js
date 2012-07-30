7ded8674e81ad65faf05ed76c49df603e4292e39
Ben Smawfield <bensmawfield@googlemail.com>
2012-07-30 18:25:42 +0100

    Updated require paths

--- views/application/application.view.js
+++ views/application/application.view.js
@@ -1,20 +1,20 @@
 define([
-    'views/cards/cards.view.js'
+    '../../views/cards/cards.view.js'
 ], function (CardsView) {
     var ApplicationView = Backbone.View.extend({
-    
+
         el : $('#app'),
-    
+
         initialize : function () {
             _.bindAll(this);
             this.render();
         },
-        
+
         render : function () {
             this.cardsView = new CardsView();
             this.$el.append(this.cardsView.render().el);
         }
     });
-    
+
     return ApplicationView;
 });
