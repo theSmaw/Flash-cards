59cb5bfa649a800f4dd4c18daf22870824a94ce5
Ben Smawfield <bensmawfield@notonthehighstreet.com>
2012-11-29 16:50:29 +0000

    able to build and run in ios

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var express = require('express'),
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -20,6 +27,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+ // app.use(allowCrossDomain)
 });
 
 app.configure('development', function(){
